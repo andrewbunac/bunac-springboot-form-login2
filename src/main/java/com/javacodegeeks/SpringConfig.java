6488a1318e0ffb0d4e7e26fdef44fb57b3461cc5
Bunac, Andrew E <AndrewE.Bunac@pccw.com>
2019-03-25 18:17:40 +0800

    initial

---
+++ src/main/java/com/javacodegeeks/SpringConfig.java
@@ -0,0 +1,17 @@
+package com.javacodegeeks;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class SpringConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/mydetails").setViewName("mydetails");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
